5d355c415f5aa49f75423cd26cdd3851b02d30f8
channaveerayya <channucs0009@gmail.com>
2021-02-03 14:02:08 +0530

    routes added

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,32 @@
+import { BrowserRouter, Route, Switch } from "react-router-dom"
 import ThemeProvider from "@material-ui/core/styles/MuiThemeProvider"
 import Header from "./UI/Header"
 import theme from "./UI/Theme"
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <Header />
-      hello
+      <BrowserRouter>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={() => <h2>home</h2>} />
+          <Route exact path="/services" component={() => <h2>services</h2>} />
+          constomsoftware
+          <Route
+            exact
+            path="/mobileapps"
+            component={() => <h2>mobileapps</h2>}
+          />
+          <Route exact path="/websites" component={() => <h2>websites</h2>} />
+          <Route
+            exact
+            path="/revolutions"
+            component={() => <h2>revolutions</h2>}
+          />
+          <Route exact path="/aboutus" component={() => <h2>aboutus</h2>} />
+          <Route exact path="/contact" component={() => <h2>contact</h2>} />
+          <Route exact path="/estimate" component={() => <h2>estimate</h2>} />
+        </Switch>
+      </BrowserRouter>
     </ThemeProvider>
   )
 }
--- src/components/UI/Header.js
+++ src/components/UI/Header.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import AppBar from "@material-ui/core/AppBar"
 import Toolbar from "@material-ui/core/Toolbar"
 import useScrollTrigger from "@material-ui/core/useScrollTrigger"
@@ -6,6 +6,8 @@ import { makeStyles } from "@material-ui/styles"
 import logo from "../../assets/logo.svg"
 import Tab from "@material-ui/core/Tab"
 import Tabs from "@material-ui/core/Tabs"
+import Button from "@material-ui/core/Button"
+import { Link } from "react-router-dom"
 function ElevationScroll(props) {
   const { children } = props
   const trigger = useScrollTrigger({
@@ -23,8 +25,14 @@ const useStyles = makeStyles((theme) => ({
     ...theme.mixins.toolbar,
     marginBottom: "3em",
   },
+  logoContainer: {
+    padding: 0,
+    "&:hover": {
+      backgroundColor: "transparent",
+    },
+  },
   logo: {
-    height: "7em",
+    height: "8em",
   },
   tabContainer: {
     marginLeft: "auto",
@@ -34,23 +42,92 @@ const useStyles = makeStyles((theme) => ({
     minWidth: 10,
     marginLeft: "25px",
   },
+  button: {
+    ...theme.typography.estimate,
+    borderRadius: "50px",
+    marginLeft: "50px",
+    marginRight: "20px",
+    height: "45px",
+  },
 }))
 
 const Header = () => {
   const classes = useStyles()
+  const [value, setValue] = useState(0)
+  const handleChange = (e, value) => {
+    setValue(value)
+  }
+  useEffect(() => {
+    if (window.location.pathname === "/" && value !== 0) {
+      setValue(0)
+    } else if (window.location.pathname === "/services" && value !== 1) {
+      setValue(1)
+    } else if (window.location.pathname === "/revolutions" && value !== 2) {
+      setValue(2)
+    } else if (window.location.pathname === "/aboutus" && value !== 3) {
+      setValue(3)
+    } else if (window.location.pathname === "/contact" && value !== 4) {
+      setValue(4)
+    }
+  }, [value])
   return (
     <>
       <ElevationScroll>
         <AppBar position="fixed">
           <Toolbar disableGutters>
-            <img src={logo} alt="logo" className={classes.logo} />
-            <Tabs className={classes.tabContainer}>
-              <Tab label="Home" className={classes.tab} />
-              <Tab label="Services" className={classes.tab} />
-              <Tab label="The Revolution" className={classes.tab} />
-              <Tab label="About Us" className={classes.tab} />
-              <Tab label="Contact Us" className={classes.tab} />
+            <Button
+              disableRipple
+              component={Link}
+              to="/"
+              className={classes.logoContainer}
+              onClick={() => setValue(0)}
+            >
+              <img src={logo} alt="logo" className={classes.logo} />
+            </Button>
+            <Tabs
+              value={value}
+              onChange={handleChange}
+              className={classes.tabContainer}
+              // indicatorColor="primary"
+            >
+              <Tab
+                label="Home"
+                component={Link}
+                to="/"
+                className={classes.tab}
+              />
+              <Tab
+                label="Services"
+                component={Link}
+                to="/services"
+                className={classes.tab}
+              />
+              <Tab
+                label="The Revolution"
+                component={Link}
+                to="revolutions"
+                className={classes.tab}
+              />
+              <Tab
+                label="About Us"
+                component={Link}
+                to="aboutus"
+                className={classes.tab}
+              />
+              <Tab
+                label="Contact Us"
+                component={Link}
+                to="contact"
+                className={classes.tab}
+              />
             </Tabs>
+            <Button
+              variant="contained"
+              color="secondary"
+              className={classes.button}
+            >
+              Free Estimate
+            </Button>
           </Toolbar>
         </AppBar>
       </ElevationScroll>

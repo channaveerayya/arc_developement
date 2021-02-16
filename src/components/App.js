05c1315e5d979e840ae4a8d3a38d4e8121877d2b
channaveerayya <channucs0009@gmail.com>
2021-02-16 12:21:36 +0530

    custom software page added

--- src/components/App.js
+++ src/components/App.js
@@ -1,37 +1,35 @@
-import { BrowserRouter, Route, Switch } from "react-router-dom"
-import { ThemeProvider as MuiThemeProvider } from "@material-ui/core/styles"
-import Header from "./UI/Header"
-import theme from "./UI/Theme"
-import Footer from "./UI/Footer"
-import LandingPage from "./LandingPage"
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles'
+import Header from './UI/Header'
+import theme from './UI/Theme'
+import Footer from './UI/Footer'
+import LandingPage from './LandingPage'
+import Services from './Services'
+import CustomSoftware from './CustomSoftware'
 function App() {
   return (
     <MuiThemeProvider theme={theme}>
       <BrowserRouter>
         <Header />
         <Switch>
-          <Route exact path="/" component={LandingPage} />
-          <Route exact path="/services" component={() => <h2>services</h2>} />
+          <Route exact path='/' component={LandingPage} />
+          <Route exact path='/services' component={Services} />
           Services
+          <Route exact path='/customsoftware' component={CustomSoftware} />
           <Route
             exact
-            path="/constomsoftware"
-            component={() => <h2>constomsoftware</h2>}
-          />
-          <Route
-            exact
-            path="/mobileapps"
+            path='/mobileapps'
             component={() => <h2>mobileapps</h2>}
           />
-          <Route exact path="/websites" component={() => <h2>websites</h2>} />
+          <Route exact path='/websites' component={() => <h2>websites</h2>} />
           <Route
             exact
-            path="/revolutions"
+            path='/revolutions'
             component={() => <h2>revolutions</h2>}
           />
-          <Route exact path="/aboutus" component={() => <h2>aboutus</h2>} />
-          <Route exact path="/contact" component={() => <h2>contact</h2>} />
-          <Route exact path="/estimate" component={() => <h2>estimate</h2>} />
+          <Route exact path='/aboutus' component={() => <h2>aboutus</h2>} />
+          <Route exact path='/contact' component={() => <h2>contact</h2>} />
+          <Route exact path='/estimate' component={() => <h2>estimate</h2>} />
         </Switch>
         <Footer />
       </BrowserRouter>
---
+++ src/components/Services.js
@@ -0,0 +1,218 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { Button, Grid, Typography, useMediaQuery } from '@material-ui/core'
+import { makeStyles, useTheme } from '@material-ui/core/styles'
+import ButtonArrow from './UI/ButtonArrow'
+import mobileIcon from '../assets/mockupIcon.svg'
+import CustomSoftwareIcon from '../assets/Custom Software Icon.svg'
+import websiteIcon from '../assets/websiteIcon.svg'
+
+const useStyles = makeStyles((theme) => ({
+  learnBtn: {
+    ...theme.typography.learnBtn,
+    fontSize: '0.7rem',
+    height: 35,
+    [theme.breakpoints.down('sm')]: {
+      marginBottom: '2em',
+    },
+  },
+  specialText: {
+    fontFamily: 'Pacifico',
+    color: theme.palette.common.orange,
+  },
+  subtitle: {
+    marginBottom: '1rem',
+  },
+  icon: {
+    // marginLeft: "10em",
+    [theme.breakpoints.down('xs')]: {
+      marginLeft: 0,
+    },
+  },
+  servicesContainer: {
+    marginTop: '8em',
+    [theme.breakpoints.down('sm')]: {
+      padding: 25,
+    },
+  },
+}))
+const Services = () => {
+  const classes = useStyles()
+  const theme = useTheme()
+  const matchesSM = useMediaQuery(theme.breakpoints.down('sm'))
+  return (
+    <Grid container direction='column'>
+      <Grid
+        item
+        align={matchesSM ? 'center' : undefined}
+        style={{
+          marginLeft: matchesSM ? 0 : '5em',
+          marginTop: matchesSM ? '1em' : '2em',
+        }}
+      >
+        <Typography
+          variant='h2'
+          gutterBottom
+          align={matchesSM ? 'center' : undefined}
+        >
+          Services
+        </Typography>
+      </Grid>
+      <Grid item>
+        <Grid
+          container
+          direction='row'
+          justify={matchesSM ? 'center' : 'flex-end'}
+          className={classes.servicesContainer}
+          style={{ marginTop: matchesSM ? '1em' : '5em' }}
+        >
+          <Grid
+            item
+            style={{
+              width: matchesSM ? undefined : '35em',
+              textAlign: matchesSM ? 'center' : undefined,
+            }}
+          >
+            <Typography variant='h4'>iOS/Android App Development</Typography>
+            <Typography variant='subtitle1' className={classes.subtitle}>
+              Extend Functionality. Extend Access. Increase Engagement
+            </Typography>
+            <Typography variant='subtitle1'>
+              Integrate your web experience or create a standalone app
+              {matchesSM ? null : <br />} with either mobile platform
+            </Typography>
+            <Button
+              component={Link}
+              to='/mobileapps'
+              variant='outlined'
+              className={classes.learnBtn}
+            >
+              <span style={{ marginRight: 10 }}>Learn More </span>
+
+              <ButtonArrow
+                width={10}
+                height={10}
+                fill={theme.palette.common.blue}
+              />
+            </Button>
+          </Grid>
+          <Grid
+            item
+            style={{
+              marginRight: matchesSM ? 0 : '5em',
+            }}
+          >
+            <img
+              className={classes.icon}
+              src={mobileIcon}
+              alt='mobileIcon'
+              width='250em'
+            />
+          </Grid>
+        </Grid>
+      </Grid>
+      <Grid item>
+        <Grid
+          container
+          direction='row'
+          justify={matchesSM ? 'center' : undefined}
+          className={classes.servicesContainer}
+        >
+          <Grid
+            item
+            style={{
+              marginLeft: matchesSM ? 0 : '5em',
+              width: matchesSM ? undefined : '35em',
+              textAlign: matchesSM ? 'center' : undefined,
+            }}
+          >
+            <Typography variant='h4'>Custom Software Development</Typography>
+            <Typography variant='subtitle1' className={classes.subtitle}>
+              Save Energy. Save Time. Save money
+            </Typography>
+            <Typography variant='subtitle1'>
+              Complete Digital solutions, from investigation to{' '}
+              <span className={classes.specialText}>celebration.</span>
+            </Typography>
+            <Button
+              component={Link}
+              to='/customsoftware'
+              variant='outlined'
+              className={classes.learnBtn}
+            >
+              <span style={{ marginRight: 10 }}>Learn More </span>
+
+              <ButtonArrow
+                width={10}
+                height={10}
+                fill={theme.palette.common.blue}
+              />
+            </Button>
+          </Grid>
+          <Grid item>
+            <img
+              className={classes.icon}
+              src={CustomSoftwareIcon}
+              alt='CustomSoftwareIcon'
+              width='250em'
+            />
+          </Grid>
+        </Grid>
+      </Grid>
+      <Grid item>
+        <Grid
+          container
+          direction='row'
+          justify={matchesSM ? 'center' : 'flex-end'}
+          className={classes.servicesContainer}
+          style={{ marginBottom: '10em' }}
+        >
+          <Grid
+            item
+            style={{
+              //   marginLeft: matchesSM ? 0 : "5em",
+              width: matchesSM ? undefined : '35em',
+              textAlign: matchesSM ? 'center' : undefined,
+            }}
+          >
+            <Typography variant='h4'>Website Development</Typography>
+            <Typography variant='subtitle1' className={classes.subtitle}>
+              Reach More. Discover More. Sell More
+            </Typography>
+            <Typography variant='subtitle1'>
+              Optimized for Search Engines, built for speed{' '}
+            </Typography>
+            <Button
+              component={Link}
+              to='/websites'
+              variant='outlined'
+              className={classes.learnBtn}
+            >
+              <span style={{ marginRight: 10 }}>Learn More </span>
+              <ButtonArrow
+                width={10}
+                height={10}
+                fill={theme.palette.common.blue}
+              />
+            </Button>
+          </Grid>
+          <Grid
+            item
+            style={{
+              marginRight: matchesSM ? 0 : '5em',
+            }}
+          >
+            <img
+              className={classes.icon}
+              src={websiteIcon}
+              alt='websiteIcon'
+              width='250em'
+            />
+          </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
+  )
+}
+
+export default Services
